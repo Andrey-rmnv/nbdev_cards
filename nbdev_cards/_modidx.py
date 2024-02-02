9ea2fb7e748f43c32e70c5b8b93562a61fb5f68e
Andrey Romanov <avm.romanov@gmail.com>
2024-02-02 13:59:21 +0100

    card class init

--- nbdev_cards/_modidx.py
+++ nbdev_cards/_modidx.py
@@ -5,4 +5,7 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://Andrey-rmnv.github.io',
                 'git_url': 'https://github.com/Andrey-rmnv/nbdev_cards',
                 'lib_path': 'nbdev_cards'},
-  'syms': {'nbdev_cards.core': {'nbdev_cards.core.foo': ('core.html#foo', 'nbdev_cards/core.py')}}}
+  'syms': { 'nbdev_cards.card': { 'nbdev_cards.card.Card': ('card.html#card', 'nbdev_cards/card.py'),
+                                  'nbdev_cards.card.Card.__init__': ('card.html#card.__init__', 'nbdev_cards/card.py'),
+                                  'nbdev_cards.card.Card.__str__': ('card.html#card.__str__', 'nbdev_cards/card.py')},
+            'nbdev_cards.core': {'nbdev_cards.core.foo': ('deck.html#foo', 'nbdev_cards/core.py')}}}
